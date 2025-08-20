fc0e863878d40e8899d309cec2902e885136482e
dongdong <95842973+BGdongdong@users.noreply.github.com>
2025-08-20 16:55:43 +0800

    1

--- next.config.ts
+++ next.config.ts
@@ -20,10 +20,16 @@ const nextConfig: NextConfig = {
         hostname: '*.20250530.space',
         port: '',
         pathname: '/**',
+      },
+      // 新增允许 baby.mvpbook.cn 域名的配置
+      {
+        protocol: 'https',
+        hostname: 'baby.mvpbook.cn', // 这里直接写完整域名（不需要*，因为是固定域名）
+        port: '',
+        pathname: '/**', // 允许该域名下的所有路径
       }
     ],
   },
-  /* config options here */
 };
 
 export default nextConfig;
